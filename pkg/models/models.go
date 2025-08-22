e8d0254d1772b00c623d24c53206a40c57d6b762
Dswang <wang892947707@hotmail.com>
2025-08-22 21:41:04 +0800

    Update models.go

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -3,8 +3,9 @@ package models
 // [最终修改] 与 CFST 工具的 CSV 输出完全对应
 type DeviceResult struct {
 	Device    string  `json:"device"`
+	Operator  string  `json:"operator"` // [FIX] Added the missing Operator field
 	IP        string  `json:"ip"`
 	LatencyMs int     `json:"latency_ms"`
-	LossPct   float64 `json:"loss_pct"` // [新增] 从 CSV 获取
-	DLMBps    float64 `json:"dl_mbps"`    // [FIX] Renamed field to reflect MB/s unit
+	LossPct   float64 `json:"loss_pct"` 
+	DLMBps    float64 `json:"dl_mbps"`
 }
