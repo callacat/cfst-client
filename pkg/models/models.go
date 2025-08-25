4d42e73b9ab84535efc6cb0b01719816b6ad1413
Dswang <wang892947707@hotmail.com>
2025-08-25 22:11:08 +0800

    移除device

--- pkg/models/models.go
+++ pkg/models/models.go
@@ -9,7 +9,7 @@ type GistContent struct {
 // DeviceResult 代表单条测速结果
 // [修改] 新增了 Region 字段
 type DeviceResult struct {
-	Device    string  `json:"device"`
+	Device    string  `json:"-"`
 	Operator  string  `json:"operator"`
 	IP        string  `json:"ip"`
 	LatencyMs int     `json:"latency_ms"`
